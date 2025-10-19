87a27da0e10ec32ea5a9937307a08e9bc815c65b
George W. <00_sling_shears@icloud.com>
2025-10-18 23:17:21 -0400

    move pngs, revise blog

--- blog.js
+++ blog.js
@@ -11,21 +11,31 @@ const content = [
     title: "Test 2",
     date: "10/17/25",
     content: `<p>This is a test of separate blogposts</p>
-    <img src="./rin.png">`
+    <p>I'm just going to leave this here for shits and giggles.</p>
+    <img src="./Images/rin.png">`
   },
 
   {
-    title: "Test 4",
-    date: "10/17/25",
-    content: `<p>This is a test of separate blogposts</p>
-    <img src="./rin.png">`
+    title: "About Me",
+    date: "10/18/25",
+    content: `<p>This is going to be a blogpost until I figure out where to implement where to put this in a text document.</p>
+    <p>Hi. I code, draw, and drum in my spare time. When I don't have spare time it's usually due to work or school.</p>
+    <p>I also run my school's one and only coding club, powered by and partnered with Hack Club (shoutout to their great programs btw)</p>
+    <p>For now, here's a rendition of my OC, drawn by a friend.</p>
+    <img src="./Images/bubblee_touhou_engie.png">
+   `
   },
 
   {
-    title: "Test 3",
-    date: "10/17/25",
-    content: `<p>This is a test of separate blogposts</p>
-    <img src="./rin.png">`
+    title: "Contact Me",
+    date: "10/18/25",
+    content: `<p>While I get the built-in email functionality working, you can contact me through other means:</p>
+    <p>Discord: @touhouengie</p>
+    <p>E-mail: natsuki.ddlc1@outlook.com</p>
+    <p>(you thought you were getting my real email there, were you? Nah, this is a burner email)</p>
+    <p>Reddit: smirbymariecallie1 (I rarely check this anymore)</p>
+    <p>I do NOT operate on Twitter nor Facebook.</p>
+    `
   }
 ];
 
