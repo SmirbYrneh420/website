e376cc0fa054a8bb6d30e46e2a61af59da998b90
George W. <110212580+TouhouEngie@users.noreply.github.com>
2025-10-22 09:21:53 -0400

    Github pages is weird

--- arrays/music.js
+++ arrays/music.js
@@ -94,13 +94,6 @@ const playlist = [
         image: "./Images/teto_chan.png",
         author: "??? feat. Kasane Teto",
         file: "./mp3s/ochamekinou.mp3"
-    },
-    {
-        title: "This is a broken entry",
-        image: "./Images/music.png",
-        author: "None",
-        file: "./mp3s/ochamekinou.mp3"
-    }
 ];
 
 window.playlist = playlist;
