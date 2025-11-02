a697f1d21c7fd4fba0f6a29b780041ce672716e3
George W. <00_sling_shears@icloud.com>
2025-11-02 14:53:55 -0500

    update blog

--- arrays/blog.js
+++ arrays/blog.js
@@ -50,9 +50,28 @@ const content = [
   },
 
   {
-    title: "WIP",
-    date: "10/20/25",
-    content: `<p>Coming Soon...</p>
+    title: "Halloween and Coffee",
+    date: "11/2/25",
+    content: `<p>Got quite busy there lately, so i'm just going to do a lightning round of stuff.</p>
+    <br>
+    <p>First, here's some Halloween art. It's still Dia de los Muertos today so a possessed Hatsune Miku still fits.</p>
+    <img src="./Images/mesmerizer_miku.png">
+    <p>Second, here's a pic of Daybright Coffee, which is the result of Chick-fil-A attempting to compete with Starbucks.</p>
+    <img src="./Images/daybright.png">
+    <p>Fun fact: I rarely consume caffeine outside of the occasional Coca-Cola, especially since not even adding milk, sugar, or cream to coffee makes it any more bearable to drink.</p>
+    <p>This was also when I realized that cappuchinos were not supposed to be sweet out of the box, and that Racetrac had fooled my family for years.</p>
+    <p>Well, at least the hot vanilla latte gave me an enjoyable drink that gave me the energy to pull through my evening shift that day.</p>
+    <br>
+    <p>Let's move to a roadmap of what I have planned for this site.</p>
+    <p>Currently, I'm working on the bulletin board, which will allow for you readers to send public inquiries to me.</p>
+    <p>Don't worry, I still have plans to finish the Gallery. More details to come about that.</p>
+    <p>However, the next two weeks are going to be a doozy with plans ranging from extracurriculars to group projects.</p>
+    <p>Other features planned for this website include:</p>
+    <ul>
+      <li>A web browser within a web browser. It's a dumb idea and I love it.</li>
+      <li>An earlier alpha version (before the reset) had a terminal mockup, which I planned (and still plan) to write in and learn React with.</li>
+    </ul>
+    <p>That's all for now. See you all in two weeks when I have the time to write another blogpost.</p>
     `
   }
 ];
