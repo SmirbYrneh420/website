dc3d23e071634e609d107ccbc1c58328325b4e58
George W. <00_sling_shears@icloud.com>
2025-11-20 14:21:37 -0500

    partial revert

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from browser import document, html
 finale = document["realresult"]
-calc = html.TABLE(id="calctable")
+calc = html.TABLE()
 calc <= html.TR(html.TH(html.DIV("0", id="result"), colspan=3) + html.TD("C"))
 lines = ["789", "456", "123", "0*=", "PXV"]
 calc <= (html.TR(html.TD(x) for x in line) for line in lines)
